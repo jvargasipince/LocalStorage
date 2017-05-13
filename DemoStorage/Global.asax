d482583cc2458e6d291fefc673d3c4d5d9ae4d70
jvargasipince <jvargas.ipince@gmail.com>
2017-05-12 20:19:11 -0500

    Add project files.

---
+++ DemoStorage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoStorage.Global" Language="C#" %>
---
+++ DemoStorage/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoStorage.Startup))]
+namespace DemoStorage
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
